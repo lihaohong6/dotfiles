e97a263807086ce2ffd01bec28e339459b61f421
Peter Li <lihaohong6@gmail.com>
2025-01-11 18:51:54 -0500

    Wezterm: windows-specific settings

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,36 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder();
+
 config.initial_cols = 120;
-config.initial_rows = 30;
-config.color_scheme = "Catppuccin Mocha";
+
+if string.find(wezterm.target_triple, "darwin") then
+    config.initial_rows = 30;
+else
+    config.initial_rows = 24;
+end
+
+config.color_scheme = 'Catppuccin Mocha';
+
+if string.find(wezterm.target_triple, 'windows') then
+    config.default_prog = { 'powershell.exe' }
+    config.launch_menu = {
+        {
+            label = "PowerShell",
+            args = { "powershell.exe", "-nol" },
+        },
+        {
+            label = "Cygwin",
+            args = { "D:\\cygwin\\bin\\zsh.exe", "--login" }
+        },
+        {
+            label = "msys2 - ucrt64",
+            args = { "cmd.exe", "/k", "D:/msys2/msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell zsh"}
+        }
+    }
+    config.font = wezterm.font_with_fallback {
+        'JetBrains Mono',
+        'Microsoft YaHei'
+    }
+end
+
 return config
