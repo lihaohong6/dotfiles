83ef154f38bb9dd9428838fb17a77e6640f658a5
Peter Li <lihaohong6@gmail.com>
2025-01-25 15:14:09 -0500

    Clear selection after copying

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -75,9 +75,10 @@ if not is_mac then
             selection_text = window:get_selection_text_for_pane(pane)
             is_selection_active = string.len(selection_text) ~= 0
             if is_selection_active then
-                window:perform_action(wezterm.action.CopyTo('ClipboardAndPrimarySelection'), pane)
+                window:perform_action(wezterm.action.CopyTo('ClipboardAndPrimarySelection'), pane);
+                window:perform_action(act.ClearSelection, pane);
             else
-                window:perform_action(wezterm.action.SendKey{ key='c', mods='CTRL' }, pane)
+                window:perform_action(wezterm.action.SendKey{ key='c', mods='CTRL' }, pane);
             end
         end),
     });
